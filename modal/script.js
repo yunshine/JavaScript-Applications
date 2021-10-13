69bf33e17f0b1dd00a3966e7c6e5d6582914319c
Yun Chung <yun.chung@mac.com>
2021-10-13 09:14:48 +0900

    modal javascript functionality finished

--- modal/script.js
+++ modal/script.js
@@ -1,3 +1,12 @@
-const test = () => {
-    console.log("testing...")
+const modal = document.querySelector("#modal");
+const overlay = document.querySelector("#overlay");
+
+const openModal = () => {
+    modal.classList.add("active");
+    overlay.classList.add("active");
+}
+
+const closeModal = () => {
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
 }
