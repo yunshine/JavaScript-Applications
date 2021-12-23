6daad54647284e70e39fdd83207c0321df51680f
Yun Chung <yun.chung@mac.com>
2021-12-23 13:25:44 +0900

    user registration logic

--- Authentication With Sessions and Cookies/routes/users.js
+++ Authentication With Sessions and Cookies/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 // add in the correct models...
 const User = require('../models/user');
@@ -12,126 +13,22 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
-router.post('/register', (req, res) => { });
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
 
-// // UPDATE User - bookmarks one cafe to the user model...
-// // Q: first, before bookmarking, is the user logged in? Use middleware...
-// router.put("/users/:userID/bookmark/:cafeID", middleware.isLoggedIn, (req, res) => {
+    // when a user registers, we need to make sure that this user doesn't already exist in the database. We'll do this by looking for the email address in the database...
+    let user = await User.findOne({ email });
+    if (user) {
+        console.log("Sorry. This email already exists in the database.")
+        return res.redirect('/register');
+    }
 
-//     Cafe.findById(req.params.cafeID, function (err, foundCafe) {
-//         if (err) {
-//             console.log(error);
-//             req.flash('error', "There was an error, and that cafe could not be found...");
-//             res.redirect(`cafes/${req.params.cafeID}`);
-//         } else {
-//             // middleware has already checked if the user is logged in...
-//             let newUser = {};
-
-//             User.findByIdAndUpdate(req.params.userID, newUser, function (err, updatedUser) {
-//                 updatedUser.bookmarks.push(foundCafe);
-//                 updatedUser.save();
-//                 if (err) {
-//                     console.log(error);
-//                     req.flash('error', "That user could not be found.");
-//                     res.redirect(`/ users / ${req.params.userID}`);
-//                 } else {
-//                     req.flash('success', "That cafe has been bookmarked.");
-//                     res.redirect(`/users/${req.params.userID}`);
-//                 }
-//             });
-//         }
-//     });
-// });
-
-// // UPDATE User - deletes a bookmarks from the user model...
-// // Q: first, before deleting the bookmark, is user logged in? Use middleware...
-// router.put("/users/:userID/deletebookmark/:cafeID", middleware.isLoggedIn, (req, res) => {
-
-//     Cafe.findById(req.params.cafeID, function (err, foundCafe) {
-//         if (err) {
-//             console.log(error);
-//             req.flash('error', "There was an error, and that cafe could not be found...");
-//             res.redirect(`/users/${req.params.userID}`);
-//         } else {
-//             // middleware has already checked if the user is logged in...
-//             let newUser = {};
-
-//             User.findByIdAndUpdate(req.params.userID, newUser, function (err, updatedUser) {
-//                 let indexToDelete;
-
-//                 for (let i = 0; i < updatedUser.bookmarks.length; i++) {
-//                     if (updatedUser.bookmarks[i]._id.toString() === req.params.cafeID.toString()) {
-//                         indexToDelete = i;
-//                         break;
-//                     }
-//                 };
-
-//                 updatedUser.bookmarks.splice(indexToDelete, 1);
-//                 updatedUser.save();
-//                 if (err) {
-//                     console.log(error);
-//                     req.flash('error', "That user could not be found.");
-//                     res.redirect(`/users/ ${req.params.userID}`);
-//                 } else {
-//                     req.flash('success', "That bookmark has been deleted.");
-//                     res.redirect(`/users/${req.params.userID}`);
-//                 }
-//             });
-//         }
-//     });
-// });
-
-// // UPDATE User - deletes a bookmarks from the user model from the cafe show page...
-// // Q: first, before deleting the bookmark, is user logged in? Use middleware...
-// router.put("/users/:userID/cafebookmarkdelete/:cafeID", middleware.isLoggedIn, (req, res) => {
-
-//     Cafe.findById(req.params.cafeID, function (err, foundCafe) {
-//         if (err) {
-//             console.log(error);
-//             req.flash('error', "There was an error, and that cafe could not be found...");
-//             res.redirect(`/users/${req.params.userID}`);
-//         } else {
-//             // middleware has already checked if the user is logged in...
-//             let newUser = {};
-
-//             User.findByIdAndUpdate(req.params.userID, newUser, function (err, updatedUser) {
-//                 let indexToDelete;
-
-//                 for (let i = 0; i < updatedUser.bookmarks.length; i++) {
-//                     if (updatedUser.bookmarks[i]._id.toString() === req.params.cafeID.toString()) {
-//                         indexToDelete = i;
-//                         break;
-//                     }
-//                 };
-
-//                 updatedUser.bookmarks.splice(indexToDelete, 1);
-//                 updatedUser.save();
-//                 if (err) {
-//                     console.log(error);
-//                     req.flash('error', "That user could not be found.");
-//                     res.redirect(`/users/ ${req.params.userID}`);
-//                 } else {
-//                     req.flash('success', "Your bookmark has been deleted.");
-//                     res.redirect(`/cafes/${req.params.cafeID}`);
-//                 }
-//             });
-//         }
-//     });
-// });
-
-
-// // SHOW Route - shows one user
-// router.get("/users/:id", (req, res) => {
-//     User.findById(req.params.id).populate("bookmarks").exec(function (err, foundUser) {
-//         if (err) {
-//             console.log(err);
-//             req.flash('error', "There was an error, and that user could not be found...");
-//             res.redirect("/cafes");
-//         } else {
-//             // renders the show page view with the one user from the DB
-//             res.render("users/show.ejs", { user: foundUser });
-//         }
-//     });
-// });
+    // if this user doesn't already exist in the database, we need to save this new user to the database...
+    user = new User({
+        username: username,
+        email: email,
+        password: "something" // we don't want to save this string password in our database, so we'll use bcrypt...
+    });
+});
 
 module.exports = router;
