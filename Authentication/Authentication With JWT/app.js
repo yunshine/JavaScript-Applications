a4158be1de8f97fb94e98ac9fd4b32f14ec2ee4a
Yun Chung <yun.chung@mac.com>
2021-12-28 12:15:22 +0900

    finished app.js server file

---
+++ Authentication/Authentication With JWT/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const connectDB = require('./config/db'); // mongoose settings found in the config folder
+
+const app = express();
+connectDB();
+
+app.use(express.json()); // Needed for incoming POST and PUT requests, because in both these requests you are sending data (in the form of some data object) to the server and you are asking the server to accept or store that data (object), which is enclosed in the body (i.e. req.body) of that (POST or PUT) Request
+
+app.use()
+app.use()
+
+app.listen(3000, () => {
+    console.log("Welcome to Authentication With JWT! You've created a server using Express. The server has started and is now listening on port 3000...");
+});
