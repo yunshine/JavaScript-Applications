bc1a0438be251af1c15212e9754b9ef1113533ea
Yun Chung <yun.chung@mac.com>
2021-12-28 12:20:01 +0900

    added middleware for routes and created route files

--- Authentication/Authentication With JWT/app.js
+++ Authentication/Authentication With JWT/app.js
@@ -7,8 +7,9 @@ connectDB();
 
 app.use(express.json()); // Needed for incoming POST and PUT requests, because in both these requests you are sending data (in the form of some data object) to the server and you are asking the server to accept or store that data (object), which is enclosed in the body (i.e. req.body) of that (POST or PUT) Request
 
-app.use()
-app.use()
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
 
 app.listen(3000, () => {
     console.log("Welcome to Authentication With JWT! You've created a server using Express. The server has started and is now listening on port 3000...");
