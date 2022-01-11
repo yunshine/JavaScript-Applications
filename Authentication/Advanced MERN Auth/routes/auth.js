be35a9e19bf555cd0739e23fbfa8f3c80cc4a8d9
Yun Chung <yun.chung@mac.com>
2022-01-11 11:48:35 +0900

    created auth routes

--- Authentication/Advanced MERN Auth/routes/auth.js
+++ Authentication/Advanced MERN Auth/routes/auth.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { register } = require('../controllers/auth');
+const { register, login, forgotPassword, resetPassword } = require('../controllers/auth');
 
-router.post('/register', register);
+router.post('/register', register); // can also be written as router.route('/register').post(register);
+
+router.post('/login', login);
+
+router.post('/forgotpassword', forgotPassword);
+
+router.post('/resetpassword/:resetToken', resetPassword);
 
 module.exports = router;
