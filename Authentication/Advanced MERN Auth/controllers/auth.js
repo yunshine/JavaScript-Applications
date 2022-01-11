9c8f61f26e9b4f8f0bde670e30448258cac2d8a9
Yun Chung <yun.chung@mac.com>
2022-01-11 14:12:11 +0900

    register controller function with notes

--- Authentication/Advanced MERN Auth/controllers/auth.js
+++ Authentication/Advanced MERN Auth/controllers/auth.js
@@ -1,12 +1,26 @@
-exports.register = (req, res, next) => {
-    res.send("Register Route");
-}
+const User = require('../models/User'); // add in correct models...
+
+exports.register = async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    try {
+        // to register a user, we will create (and save) a new user to the database. Checking to see if the user already exists and hashing of the password prior to saving will take place in middleware.
+        const user = await User.create({username, email, password});
+
+        res.status(201).json({ success: true, user: user }); // A 201 status code indicates that the request has succeeded and has led to the creation of a NEW resource
+    } catch (error) {
+
+    }
+};
+
 exports.login = (req, res, next) => {
     res.send("Login Route");
-}
+};
+
 exports.forgotPassword = (req, res, next) => {
     res.send("Forgot Password Route");
-}
+};
+
 exports.resetPassword = (req, res, next) => {
     res.send("Reset Password Route");
-}
+};
