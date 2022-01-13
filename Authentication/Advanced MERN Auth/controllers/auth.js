0b2509829f5d2c71e0732e62cfe39f52627faae0
Yun Chung <yun.chung@mac.com>
2022-01-13 12:14:47 +0900

    tested try catch function and added return statements

--- Authentication/Advanced MERN Auth/controllers/auth.js
+++ Authentication/Advanced MERN Auth/controllers/auth.js
@@ -7,9 +7,9 @@ exports.register = async (req, res, next) => {
         // to register a user, we will create (and save) a new user to the database. Checking to see if the user already exists and hashing of the password prior to saving will take place in middleware.
         const user = await User.create({ username, email, password });
 
-        res.status(201).json({ success: true, user: user }); // A 201 status code indicates that the request has succeeded and has led to the creation of a NEW resource
+        return res.status(201).json({ success: true, user: user }); // A 201 status code indicates that the request has succeeded and has led to the creation of a NEW resource
     } catch (error) {
-        console.log("There was an error in the registration process: ", error);
+        console.log("There was a server error in the registration process: ", error);
         res.status(500).json({ success: false, error: error.message }); // A 500 status code indicates that there was an internal server error
     }
 };
@@ -19,7 +19,7 @@ exports.login = async (req, res, next) => {
 
     // check on the server side if the user has typed in an email and password
     if (!email || !password) {
-        res.status(400).json({ success: false, error: "Please provide an email address and password." }); // The res. status() function sets the HTTP status for the response; A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
+        return res.status(400).json({ success: false, error: "Please provide an email address and password." }); // The res. status() function sets the HTTP status for the response; A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
     }
 
     // does this user exist in our database?
@@ -28,7 +28,7 @@ exports.login = async (req, res, next) => {
 
         // if the email provided by the user does not exist...
         if (!user) {
-            res.status(400).json({ success: false, error: "Sorry. That email address or password is incorrect." }); // The res. status() function sets the HTTP status for the response; A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
+            return res.status(400).json({ success: false, error: "Sorry. That email address or password is incorrect." }); // The res. status() function sets the HTTP status for the response; A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
         }
 
         // if the email address matches, bcryptjs is used to compare the password in the req.body to the password in the database...
@@ -37,13 +37,13 @@ exports.login = async (req, res, next) => {
         // if the password provided by the user does not match the password in the database...
         if (!isMatch) {
             console.log("Sorry. That email address or password is incorrect.")
-            res.status(400).json({ success: false, error: "Sorry. That email address or password is incorrect." }); // The res. status() function sets the HTTP status for the response; A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
+            return res.status(400).json({ success: false, error: "Sorry. That email address or password is incorrect." }); // The res. status() function sets the HTTP status for the response; A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
         }
 
         // if we make it past the if statements to this point in the code, I want to respond with a token and let the user log in
-        res.status(200).json({ success: true, token: 'random-gibberish-for-now' }); // A 200 status code indicates that the request has succeeded (depending on the HTTP request method)...
+        return res.status(200).json({ success: true, token: 'random-gibberish-for-now' }); // A 200 status code indicates that the request has succeeded (depending on the HTTP request method)...
     } catch (error) {
-        console.log("There was an error in the login process: ", error);
+        console.log("There was a server error in the login process: ", error);
         res.status(500).json({ success: false, error: error.message }); // A 500 status code indicates that there was an internal server error
     }
 };
