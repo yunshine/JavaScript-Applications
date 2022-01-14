94b74db5c24f513a4dbb96557f29892452a74daf
Yun Chung <yun.chung@mac.com>
2022-01-14 10:44:53 +0900

    completed errorHandler middleware

--- Authentication/Advanced MERN Auth/middleware/error.js
+++ Authentication/Advanced MERN Auth/middleware/error.js
@@ -4,6 +4,7 @@ const errorHandler = (err, req, res, next) => {
     let error = { ...err };
 
     error.message = err.message;
+    console.log("err from error Handler: ", err);
 
     if (err.code === 11000) {  // in Mongoose, 11000 is a duplicate key error
         const message = 'Duplicate Field Value Error';
@@ -14,4 +15,8 @@ const errorHandler = (err, req, res, next) => {
         const message = Object.values(err.errors).map(value => value.message);
         error = new ErrorResponse(message, 400); // A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
     }
+
+    res.status(error.statusCode || 500).json({ success: false, error: error.message || "Server Error." }); // A 500 status code indicates that there was an internal server error
 };
+
+module.exports = errorHandler;
