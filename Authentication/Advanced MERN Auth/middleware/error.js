fc3888432967f54c5b1ac9f8ed687a762c69c5a0
Yun Chung <yun.chung@mac.com>
2022-01-14 10:40:19 +0900

    error check for ValidationError in middleware

--- Authentication/Advanced MERN Auth/middleware/error.js
+++ Authentication/Advanced MERN Auth/middleware/error.js
@@ -1,12 +1,17 @@
 const ErrorResponse = require('../utilities/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
-    const error = { ...err };
+    let error = { ...err };
 
-    const message = error.message;
+    error.message = err.message;
 
-    if (error.code === 11000) {  // in Mongoose, 11000 is a duplicate key error
+    if (err.code === 11000) {  // in Mongoose, 11000 is a duplicate key error
         const message = 'Duplicate Field Value Error';
-        error = new ErrorResponse(message, 400);
+        error = new ErrorResponse(message, 400); // A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
+    }
+
+    if (err.name === "ValidationError") {
+        const message = Object.values(err.errors).map(value => value.message);
+        error = new ErrorResponse(message, 400); // A 400 status code indicates that the server can't or won't process the request due to something that is perceived to be a client error
     }
 };
