fac0888fec8b57592bf4084edb93ffb1919baf30
Yun Chung <yun.chung@mac.com>
2022-01-27 11:01:32 +0900

    login screen route using react-router-dom v6

--- Authentication/Advanced MERN Auth/client/src/App.js
+++ Authentication/Advanced MERN Auth/client/src/App.js
@@ -1,10 +1,20 @@
-import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginScreen from './components/screens/LoginScreen';
 
 function App() {
     return (
-        <div className="App">
-            hi welcome to the app
-        </div>
+        <Router>
+            <div className="App">
+                hi welcome to the app
+                {/* <Switch> // react-router-dom v. 5 syntax... */}
+                {/* <Route exact path="/login" element={LoginScreen} />  // react-router-dom v. 5 syntax... */}
+                <Routes>
+
+                    <Route path="/" element={<LoginScreen />} />
+                </Routes>
+                {/* </Switch> // react-router-dom v. 5 syntax... */}
+            </div>
+        </Router>
     );
 }
 
---
+++ Authentication/Advanced MERN Auth/client/src/components/screens/LoginScreen.js
@@ -0,0 +1,9 @@
+const LoginScreen = () => {
+    return (
+        <div className="LoginScreen">
+            <h1>loginscreen...</h1>
+        </div>
+    );
+}
+
+export default LoginScreen;
