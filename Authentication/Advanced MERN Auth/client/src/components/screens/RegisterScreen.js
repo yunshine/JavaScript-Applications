49324f5f111b4a6ef6678608fe1513520da650ef
Yun Chung <yun.chung@mac.com>
2022-01-31 11:11:56 +0900

    registration page html

--- Authentication/Advanced MERN Auth/client/src/components/screens/RegisterScreen.js
+++ Authentication/Advanced MERN Auth/client/src/components/screens/RegisterScreen.js
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios'; // JS library used to make HTTP requests from node. js; supports the Promise API
+
 const RegisterScreen = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     return (
         <div className="RegisterScreen">
-            <h1>registerscreen...</h1>
+            <form className="RegisterScreen-form">
+                <h3 className="RegisterScreen-from-title">Register</h3>
+                <div className="form-group">
+                    <label htmlFor="name">Username:</label>
+                    <input type="text" required id="name" placeholder="Enter Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="email">Email Address:</label>
+                    <input type="email" required id="email" placeholder="Enter Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" required id="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="confirmPassword">Confirm Password:</label>
+                    <input type="password" required id="confirmPassword" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                </div>
+
+                <button type="submit">Register</button>
+
+                <p>Already have an account? <Link to="/login">Login</Link></p>
+            </form>
         </div>
     );
 }
