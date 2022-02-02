10560e9760f153d54784b2a5cc937d07c3779424
Yun Chung <yun.chung@mac.com>
2022-02-02 17:23:51 +0900

    forgotPasswordScreen component imports and states

--- Authentication/Advanced MERN Auth/client/src/components/screens/ForgotPasswordScreen.js
+++ Authentication/Advanced MERN Auth/client/src/components/screens/ForgotPasswordScreen.js
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 const ForgotPasswordScreen = () => {
+    const [email, setEmail] = useState("")
+    const [error, setError] = useState("")
+    const [success, setSuccess] = useState("")
+
     return (
         <div className="ForgotPasswordScreen">
             <h1>ForgotPasswordscreen...</h1>
