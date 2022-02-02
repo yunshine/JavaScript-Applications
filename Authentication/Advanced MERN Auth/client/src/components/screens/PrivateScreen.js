f98af02e899c05d10dc90c642811ea3fdcc42e57
Yun Chung <yun.chung@mac.com>
2022-02-02 17:08:44 +0900

    privateScreen component - useEffect to redirect if user is not logged in

--- Authentication/Advanced MERN Auth/client/src/components/screens/PrivateScreen.js
+++ Authentication/Advanced MERN Auth/client/src/components/screens/PrivateScreen.js
@@ -1,4 +1,17 @@
-const PrivateScreen = () => {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PrivateScreen = ({ history }) => {
+    const [error, setError] = useState("");
+    const [privateData, setPrivateData] = useState("");
+
+    useEffect(() => {
+        // if there's no authToken in the localStorage, the user is not logged in, so we want to immediately redirect
+        if (!localStorage.getItem("authToken")) {
+            history.push("/login");
+        }
+    }, []);
+
     return (
         <div className="PrivateScreen">
             <h1>Privatescreen...</h1>
