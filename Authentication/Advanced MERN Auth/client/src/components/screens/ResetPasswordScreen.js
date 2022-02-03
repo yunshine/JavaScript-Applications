9bb176e5d11e912d4de2a9cfbcc21e9339c6b675
Yun Chung <yun.chung@mac.com>
2022-02-03 10:25:00 +0900

    resetpasswordscreen component resetPassword function logic

--- Authentication/Advanced MERN Auth/client/src/components/screens/ResetPasswordScreen.js
+++ Authentication/Advanced MERN Auth/client/src/components/screens/ResetPasswordScreen.js
@@ -8,6 +8,29 @@ const ResetPasswordScreen = () => {
     const [error, setError] = useState("");
     const [success, setSuccess] = useState("");
 
+    const resetPasswordHandler = async (e) => {
+        e.preventDefault();
+
+        const config = { headers: { "Content-Type": "applicaiton/json" } };
+
+        if (password !== confirmPassword) {
+            setPassword("");
+            setConfirmPassword("");
+            setTimeout(() => {
+                setError("");
+            }, 5000);
+        }
+        return setError("Passwords do not match.");
+
+        try {
+            const { data } = await axios.put(`/api/auth/passwordreset/${match.params.resetToken}`, { password }, config);
+            console.log(data);
+            setSuccess(data.data);
+        } catch (error) {
+
+        }
+    };
+
     return (
         <div className="ResetPasswordScreen">
             <h1>ResetPasswordscreen...</h1>
