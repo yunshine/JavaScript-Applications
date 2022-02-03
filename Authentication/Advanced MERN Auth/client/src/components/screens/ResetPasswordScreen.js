8dd91ebe75bc17d15827d6e71c3306292040fcff
Yun Chung <yun.chung@mac.com>
2022-02-03 10:19:48 +0900

    resetPasswordScreen component imports and states

--- Authentication/Advanced MERN Auth/client/src/components/screens/ResetPasswordScreen.js
+++ Authentication/Advanced MERN Auth/client/src/components/screens/ResetPasswordScreen.js
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 const ResetPasswordScreen = () => {
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
     return (
         <div className="ResetPasswordScreen">
             <h1>ResetPasswordscreen...</h1>
