c77f1f95f8d43028843aa366ac7b1d4a108c7109
Yun Chung <yun.chung@mac.com>
2022-02-04 11:18:52 +0900

    privateScreen after bug fixes

--- Authentication/Advanced MERN Auth/client/src/components/routing/PrivateRoute.js
+++ Authentication/Advanced MERN Auth/client/src/components/routing/PrivateRoute.js
@@ -1,10 +1,8 @@
-import { Navigate, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
     return (
-
         localStorage.getItem("authToken") ? children : <Navigate to="/login" />
-
     );
 };
 
